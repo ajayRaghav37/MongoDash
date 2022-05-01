316e3efffc6bd05092fb81f3ecdd07675949cc13
Ajay RAGHAV <ajay.raghav@mongodb.com>
2022-05-01 13:02:03 +0530

    Added data files to be used and updated prepData.js

--- src/prepData.js
+++ src/prepData.js
@@ -3,7 +3,7 @@ const initDate = new Date('2018-01-29');
 const initTime = initDate.getTime();
 const csvToJson = require('csvtojson');
 const fs = require('fs');
-const {Parser} = require('json2csv');
+const { Parser } = require('json2csv');
 const step = 20 * 24 * 60 * 60 * 1000;
 const countries = ["Abkhazia", "Afghanistan", "Aland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica",
     "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus",
@@ -28,6 +28,15 @@ const countries = ["Abkhazia", "Afghanistan", "Aland Islands", "Albania", "Alger
     "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna",
     "Western Sahara", "Yemen", "Zambia", "Zimbabwe"];
 
+const freqs = [];
+let freqSum = 0;
+
+for (let i = 0; i < countries.length; i++) {
+    const freq = Math.random() * 100;
+    freqs.push(freq);
+    freqSum += freq;
+}
+
 const freqCountries = ["Pakistan", "India", "Bangladesh", "Philippines", "Nepal", "Nigeria", "Kenya"];
 const data2018 = [];
 const data2019 = [];
@@ -46,7 +55,28 @@ csvToJson().fromFile('./data.csv').then(
             delete data.step;
             data.isFraud = data.isFraud === '1';
             data.isFlaggedFraud = data.isFlaggedFraud === '1';
-            data.country = Math.floor(Math.random() * 5) === 0 ? freqCountries[Math.floor(Math.random() * freqCountries.length)] : countries[Math.floor(Math.random() * countries.length)];
+
+            const randomFreq = Math.random() * freqSum;
+
+            const findIndex = f => {
+                let currSum = 0;
+
+                for (let i = 0; i < freqs.length; i++) {
+                    const currF = freqs[i];
+                    currSum += currF;
+
+                    if (currSum >= f) {
+                        return i;
+                    }
+                }
+            };
+
+            data.country = Math.random() * 25 < 1 ? freqCountries[Math.floor(Math.random() * freqCountries.length)] :
+                countries[findIndex(randomFreq)];
+
+            if (!data.country) {
+                console.log(randomFreq, freqSum, freqs);
+            }
 
             if (data.date.getFullYear() === 2018) {
                 data2018.push(data);
@@ -68,6 +98,11 @@ csvToJson().fromFile('./data.csv').then(
             fs.writeFileSync('2018.csv', parser.parse(data2018));
             fs.writeFileSync('2019.csv', parser.parse(data2019));
             fs.writeFileSync('2020.csv', parser.parse(data2020));
+            const parserF = new Parser();
+            fs.writeFileSync('freqs.csv', parserF.parse(countries.map(x => ({
+                country: x,
+                freq: rawData.filter(y => y.country === x).length
+            }))));
         } catch (err) {
             console.error(err);
         }
